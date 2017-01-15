447a2508602e892c0649a498dbd32089ff05604e
FRAJ Saber <frejsaber@gmail.com>
2017-01-15 14:08:02 +0100

    ajouter le modele User

---
+++ API-WEB_AngularJS/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_WEB_AngularJS.Models
+{
+    class User
+    {
+        public int ID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+    }
+}
